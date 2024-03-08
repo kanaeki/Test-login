a9e6199559d9065368f0c40397cc72445e93b530
kanaeki <hamzaistic2002@gmail.com>
2024-03-08 23:20:40 +0500

    to deploy

--- src/Router/user.Route.js
+++ src/Router/user.Route.js
@@ -5,7 +5,6 @@ const  userController  =require('../Controller/user.Controller')
 
 
 uroute.post('/signup',userController.Register)
-uroute.get('/',userController.check)
 uroute.post('/login',userController.logIn)
 uroute.post('/forgot',userController.forgetPassword)
 
